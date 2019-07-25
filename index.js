4a5014daf3088cb7bfb06bb05afbb5d028282ba6
Cedricium <amaya.cedric@gmail.com>
2019-07-24 18:40:31 -0700

    Setup minimal Express server

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// implement your API here
+const express = require('express')
+const app = express()
+const db = require('./data/db')
+
+const PORT = process.env.PORT || 5000
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.json({
+    message: `Mr. Watson, come here. I want to see you.`
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`server listening on port ${PORT}`)
+})
